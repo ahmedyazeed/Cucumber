f5f5a35c94efbe5edd80cd618e444b67c72def28
ahmedyazeed86 <ahmedyazeed86@gmail.com>
2023-10-17 22:01:18 -0400

    class03

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,10 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
+public class CommonMethods {
+    public static WebDriver driver;
+  // driver.manage().timeouts).implicitlyWait(Duration.ofSeconds(20));
+}
