70bd0304bd56e0dd9e8265a0ab2dd24e082d04c9
ahmedyazeed86 <ahmedyazeed86@gmail.com>
2023-10-24 22:10:16 -0400

    class06

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,26 +1,29 @@
 package utils;
 
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.cucumber.java.et.Ja;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
-public class CommonMethods {
+public class CommonMethods extends PageInitializer {
 
     public static WebDriver driver;
 
     public static void openBrowserAndLaunchApplication(){
         ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
         switch (ConfigReader.getPropertyValue("browser")){
-            case "chrome":
+            case "Chrome":
                 driver = new ChromeDriver();
                 break;
             case "firefox":
@@ -31,6 +34,7 @@ public class CommonMethods {
         }
         driver.manage().window().maximize();
         driver.get(ConfigReader.getPropertyValue("url"));
+        initializePageObjects();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Constants.IMPLICIT_WAIT));
     }
 
@@ -64,6 +68,38 @@ public class CommonMethods {
         getJSExecutor().executeScript("arguments[0].click();", element);
     }
 
+    public void closeBrowser(){
+        driver.quit();
+    }
 
+    //take screenshot method for capturing all the screenshots
+    public static byte[] takeScreenshot(String fileName){
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        //in cucumber screenshot should be taken in array of byte format
+        //since the size of the image is in byte, that's why the return type of this
+        //method is array of byte
+        byte[] picByte = ts.getScreenshotAs(OutputType.BYTES);
+        File sourceFile = ts.getScreenshotAs(OutputType.FILE);
+        //we will pass the path of ss from constants class
+        try {
+            FileUtils.copyFile
+                    (sourceFile, new File
+                            (Constants.SCREENSHOT_FILEPATH +
+                                    fileName+" "+
+                                    getTimeStamp("yyyy-MM-dd-HH-mm-ss")
+                                    +".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return picByte;
+    }
 
+    //in java we have a module which returns current date and time
+    public static String getTimeStamp(String pattern){
+        Date date = new Date();
+        //after getting the date, I need to format it as per my requirement
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        //it willl return the formatted date as per the pattern in string format
+        return sdf.format(date);
+    }
 }
